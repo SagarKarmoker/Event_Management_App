25bc82ea1c10edab5a929bb5ed5282e407b3356b
Sagar-Karmoker <sagarkarmoker2001@gmail.com>
2023-07-23 00:40:33 +0600

    lab 2 work done finding the ids

--- app/src/main/java/edu/ewubd/CSE489232_2020_2_60_054/CreateEventActivity.java
+++ app/src/main/java/edu/ewubd/CSE489232_2020_2_60_054/CreateEventActivity.java
@@ -1,19 +1,24 @@
 package edu.ewubd.CSE489232_2020_2_60_054;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class CreateEventActivity extends Activity {
 
     // R java generated Resource file
-
     private EditText etName, etPlace, etDate, etCapacity, etBudget, etEmail, etPhone, etDsc;
     private Button cancelBtn, shareBtn, saveBtn;
     private TextView errorTv;
+    private RadioGroup radioGroup;
+    private String radioText = "";
+    // error text
+    String err = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,9 @@ public class CreateEventActivity extends Activity {
         // textviews
         errorTv = findViewById(R.id.errorTv);
 
+        // Radio
+        radioGroup = findViewById(R.id.radioGroup);
+
 
         //etName.getText() --> return editable object
         String name = etName.getText().toString();
@@ -49,38 +57,92 @@ public class CreateEventActivity extends Activity {
         String phone = etPhone.getText().toString();
         String desc = etDsc.getText().toString();
 
-        String err = "";
+        // converting string to Integer
+        int cap, event_budget;
+        long phn;
 
-        if(name.isEmpty() || name.length() < 4 || name.length() > 12){
-            err += "Invalid Name\n";
+        if(!capacity.isEmpty() && !budget.isEmpty() && !phone.isEmpty()){
+            cap = Integer.parseInt(capacity);
+            event_budget = Integer.parseInt(budget);
+            phn = Long.parseLong(phone);
+
+            if(cap <= 0 || cap > 1000 && event_budget > 0){
+                err += "Invalid capacity\n or invalid budget";
+            }
         }
 
-        int cap = Integer.parseInt(capacity);
-        if(cap <= 0 || cap > 1000){
-            err += "Invaild capacity\n";
-        }
 
+        // error text show
         if(!err.isEmpty()){
             errorTv.setText(err);
             return;
         }
-
-
-        saveBtn.setOnClickListener(new View.OnClickListener() {
+        
+        // radio button
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                // physical event convert into software event
-                System.out.println("Save Button was clicked");
-                // 1. retrieve all field data from XML
-                // 2. check the validity of data
-                    // a. if not valid, then show error message
-                    // b. if everything ok, then save
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radioIndoor){
+                    radioText += "Indoor";
+                }
+                else if(checkedId == R.id.radioOutdoor){
+                    radioText += "Outdoor";
+                }
+                else if (checkedId == R.id.radioOnline) {
+                    radioText += "Online";
+                }
             }
         });
 
 
+        // button works
+        /*
+        * physical event convert into software event
+        * // 1. retrieve all field data from XML
+                // 2. check the validity of data
+                    // a. if not valid, then show error message
+                    // b. if everything ok, then save
+        * */
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Save Button was clicked");
 
-        // end of oncreate
+                // input field check
+                if(place.isEmpty() || date.isEmpty() || budget.isEmpty() || email.isEmpty() || phone.isEmpty() || desc.isEmpty()){
+                    if(name.isEmpty() || name.length() < 4 || name.length() > 12){
+                        err += "Invalid Name\n";
+                    }
+
+                    err += "please fill all the fields";
+                }
+
+
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("cancel btn");
+            }
+        });
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.putExtra(Intent.EXTRA_TEXT, "event details");
+                i.setType("text/plain");
+
+                startActivity(i);
+            }
+        });
+        // end of on create
+    }
+
+    // text input valid check
+    public void inputCheck(){
 
     }
 }
